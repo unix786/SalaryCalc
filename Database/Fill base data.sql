db4d7c8b58623c8eaebe02ea031398489d901215
Andrius <andriusrdk@gmail.com>
2020-03-21 22:03:17 +0200

    Scripts to create the database  - Still need to write test data.

---
+++ Database/Fill base data.sql
@@ -0,0 +1,30 @@
+INSERT Parameter (MinimumWage) VALUES (800)
+
+INSERT Bonus (Score, Value)
+VALUES
+	(0, 0),
+	(1, 1),
+	(2, 2),
+	(3, 7),
+	(4, 15),
+	(5, 20)
+
+INSERT Position (Name) VALUES ('Technician')
+INSERT BaseSalary (PositionID, Years, Value)
+SELECT @@IDENTITY, *
+FROM (
+	VALUES
+		(0, 1000),
+		(3, 1200),
+		(6, 1520)
+) V(Y, V)
+
+INSERT Position (Name) VALUES ('Sales')
+INSERT BaseSalary (PositionID, Years, Value)
+SELECT @@IDENTITY, *
+FROM (
+	VALUES
+		(0, 1100),
+		(3, 1400),
+		(7, 1650)
+) V(Y, V)
---
+++ Database/Snippets/Shift ratings.sql
@@ -0,0 +1,22 @@
+﻿DECLARE @Year int = YEAR(GETDATE()) -- Assuming the server also sets YearsEmployed.
+
+		UPDATE E
+		SET
+			E.YearsEmployed = E.YearsNow,
+			E.PrevRating1 = E.Rating,
+			E.PrevRating2 = CASE E.YearsNow - E.YearsEmployed
+				WHEN 1 THEN E.PrevRating1
+				ELSE E.Rating -- If skipped a year or more.
+			END,
+			Salary = NULL
+		FROM
+			(
+				SELECT
+					*,
+					@Year - Employed as YearsNow
+				FROM
+					Employee
+			) E
+			INNER JOIN inserted I ON I.ID = E.ID
+		WHERE
+			E.YearsEmployed <> E.YearsNow
